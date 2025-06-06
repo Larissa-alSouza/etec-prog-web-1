49fefa6a724a39c633d16012591a780d6dd72c5b
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-06-06 17:04:52 -0300

    Add files via upload

---
+++ Pokedex/script.js
@@ -0,0 +1,47 @@
+const modal = document.getElementById("modal")
+
+function buscarNome(){
+    abrir()
+    nome()
+}
+
+function abrir()
+{
+    modal.style.display = "flex"
+}
+
+function nome()
+{
+    var input = document.getElementById("nomepoke")
+
+    var url = "https://pokeapi.co/api/v2/pokemon/" + input.value
+    fetch(url)
+        .then(response => {
+            if (!response.ok) throw new Error("Pokémon não encontrado");
+            return response.json();
+        })
+
+        .then(dados => {
+            var foto = document.getElementById("foto")
+            var nome = document.getElementById("nome")
+            var id = document.getElementById("ident")
+            var altura = document.getElementById("altura")
+            var peso = document.getElementById("peso")
+            var tipo = document.getElementById("tipo")
+            var habilidades = document.getElementById("hab")
+            var estatisticas = document.getElementById("estatisticas")
+            foto.src = dados.sprites.front_default
+            nome.value = dados.name
+            id.value = dados.id
+            altura.value = dados.height
+            peso.value = dados.weight
+            tipo.value = dados.types.map(t => t.type.name)
+            habilidades.value = dados.abilities.map(a => a.ability.name)
+            estatisticas.value = dados.stats.map(stat => ({nome: stat.stat.name, valor: stat.base_stat}))
+        })
+
+        .catch(error => {
+            console.error('Erro:', error)
+        });
+
+    }
