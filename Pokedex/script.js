35ed8b5d8d426523988b6476bb8595df43adca22
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-06-11 22:24:56 -0300

    Add files via upload

---
+++ Pokedex/script.js
@@ -0,0 +1,129 @@
+const modal = document.getElementById("modal")
+
+function buscarNome(){
+    abrir()
+    nome()
+}
+
+function buscarId()
+{
+    abrir()
+    identificador()
+}
+
+function iniciarpesq()
+{
+    abrir()
+    iniciar()
+}
+
+function abrir()
+{
+    modal.style.display = "flex"
+}
+
+window.addEventListener("click", (e) => 
+    {
+        if(e.target === modal)
+        {
+            modal.style.display = "none"
+        }
+    })
+
+function nome()
+{
+    var input = document.getElementById("nomepoke")
+
+    var url = "https://pokeapi.co/api/v2/pokemon/" + input.value
+    fetch(url)
+        .then(response => response.json())
+
+        .then(dados => {
+            var foto = document.getElementById("foto")
+            var nome = document.getElementById("nome")
+            var id = document.getElementById("ident")
+            var altura = document.getElementById("altura")
+            var peso = document.getElementById("peso")
+            var tipo = document.getElementById("tipo")
+            var habilidades = document.getElementById("hab")
+            var estatisticas = document.getElementById("estatisticas")
+            foto.src = dados.sprites.front_default
+            nome.value = dados.name
+            id.value = dados.id
+            altura.value = dados.height
+            peso.value = dados.weight
+            tipo.value = dados.types.map(t => t.type.name)
+            habilidades.innerHTML = dados.abilities.map(a => a.ability.name)
+            estatisticas.value = dados.stats.map(stat => ({nome: stat.stat.name, valor: stat.base_stat}))
+        })
+
+        .catch(error => {
+            console.error('Erro:', error)
+        });
+
+}
+
+function identificador()
+{
+    var input = document.getElementById("identificador")
+
+    var url = "https://pokeapi.co/api/v2/pokemon/" + input.value
+    fetch(url)
+        .then(response => response.json())
+
+        .then(dados => {
+            var foto = document.getElementById("foto")
+            var nome = document.getElementById("nome")
+            var id = document.getElementById("ident")
+            var altura = document.getElementById("altura")
+            var peso = document.getElementById("peso")
+            var tipo = document.getElementById("tipo")
+            var habilidades = document.getElementById("hab")
+            var estatisticas = document.getElementById("estatisticas")
+            foto.src = dados.sprites.front_default
+            nome.value = dados.name
+            id.value = dados.id
+            altura.value = dados.height
+            peso.value = dados.weight
+            tipo.value = dados.types.map(t => t.type.name)
+            habilidades.value = dados.abilities.map(a => a.ability.name)
+            estatisticas.innerText = dados.stats.map(stat => ({nome: stat.stat.name, valor: stat.base_stat}))
+        })
+
+        .catch(error => {
+            console.error('Erro:', error)
+        });
+}
+
+//tentativa de fazer os botões iniciar, frente e volta
+function iniciar()
+{
+    var input = 1
+
+    var url = "https://pokeapi.co/api/v2/pokemon/" + input
+    fetch(url)
+        .then(response => response.json())
+
+        .then(dados => {
+            var foto = document.getElementById("foto")
+            var nome = document.getElementById("nome")
+            var id = document.getElementById("ident")
+            var altura = document.getElementById("altura")
+            var peso = document.getElementById("peso")
+            var tipo = document.getElementById("tipo")
+            var habilidades = document.getElementById("hab")
+            var estatisticas = document.getElementById("estatisticas")
+            foto.src = dados.sprites.front_default
+            nome.value = dados.name
+            id.value = dados.id
+            altura.value = dados.height
+            peso.value = dados.weight
+            tipo.value = dados.types.map(t => t.type.name)
+            habilidades.value = dados.abilities.map(a => a.ability.name)
+            estatisticas.innerText = dados.stats.map(stat => ({nome: stat.stat.name, valor: stat.base_stat}))
+        })
+
+        .catch(error => {
+            console.error('Erro:', error)
+        });
+}
